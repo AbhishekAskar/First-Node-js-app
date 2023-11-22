43f59ed4e32e2d5c412538d0403fb3609f0d69bd
Abhishek <abhiaskar.18@gmail.com>
2023-11-22 11:11:13 +0530

    Added size of product in the form

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
 // This line of code between const app and const server is called middleware ig
 
-app.use((req, res, next) => {
-    console.log('In the middleware');
-    next(); // Allows the request to continue to the next middleware in line
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/add-product', (req, res, next) => {
+    res.send(`
+        <form action="/product" method="POST">
+            <input type="text" name="title" placeholder="Product Title"><br>
+            <input type="text" name="size" placeholder="Product Size"><br>
+            <button type="submit">Add Product</button>
+        </form>
+    `);
+});
+app.post('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
 });
 
-app.use((req, res, next) => {
-    console.log('In another middleware');
+app.use('/',(req, res, next) => {
     res.send("<h1>Hello from Express</h1>")
 });
 
