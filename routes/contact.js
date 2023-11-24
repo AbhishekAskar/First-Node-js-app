23adddfdc015e06db944c23f5903856da9276123
Abhishek <abhiaskar.18@gmail.com>
2023-11-24 14:08:34 +0530

    updated contact us page

--- routes/contact.js
+++ routes/contact.js
@@ -8,5 +8,14 @@ const rootDir = require('../util/path');
 router.get('/contactus',(req, res, next) => {
     res.sendFile(path.join(rootDir, 'views', 'contact.html'));
 });
+router.post('/contactus', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/success');
+});
+
+router.get('/success', (req, res, next) => {
+    res.sendFile(path.join(rootDir, 'views', 'success.html'));  
+});
+
 
 module.exports = router;
